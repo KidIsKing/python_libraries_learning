5a9b76c3839b6db82f26491c3db46dd23064dd00
YanaMasalova <anulamasalova@gmail.com>
2026-08-08 18:39:16 +0600

    add initial analytics for statistics

--- pytorch/guide/practice.py
+++ pytorch/guide/practice.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+import numpy as np
 import torch
 from torch.nn import Sequential, Linear, ReLU, CrossEntropyLoss
 from torch.optim import SGD
+
+
+def info(data):
+    """Подходит для NumPy-массив."""
+    return (f"Дата: {datetime.now()}.\n"
+            f"Точность модели в % (100 тестов): {data};\n"
+            f"минимальная точность: {data.min()};\n"
+            f"максимальная число: {data.max()};\n"
+            f"средняя точность: {data.mean()};\n"
+            f"среднее отклонение точности: {data.std()}.\n\n")
+
+
+def save_info(message):
+    with open("pytorch/guide/data/info.txt", "a", encoding="utf-8") as f:
+        f.write(message)
 
 
 # Загрузка файлов
@@ -11,24 +28,31 @@ y = torch.load("pytorch/guide/data/target.pt")
 X_train, X_val = X[:100], X[100:]
 y_train, y_val = y[:100], y[100:]
 
-# Инициализация модели, функции потерь и оптимизатора для ГС
-model = Sequential(
-    Linear(4, 16),
-    ReLU(),
-    Linear(16, 3)
-)
-loss_fn = CrossEntropyLoss()
-optimizer = SGD(model.parameters(), lr=0.01)
+results = []
 
-# Обучение повторением шагов градиентного спуска
 for _ in range(100):
-    y_hat = model(X_train)
-    loss = loss_fn(y_hat, y_train)
-    loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
-    # print(f"{loss:0.3f}")
+    # Инициализация модели, функции потерь и оптимизатора для ГС
+    model = Sequential(
+        Linear(4, 16),
+        ReLU(),
+        Linear(16, 3)
+    )
+    loss_fn = CrossEntropyLoss()
+    optimizer = SGD(model.parameters(), lr=0.01)
 
-# Подсчёт эффективности модели
-score = (model(X_val).argmax(1) == y_val).sum() / len(y_val) * 100
-print(f"Точность модели: {score}%.")
+    # Обучение повторением шагов градиентного спуска
+    for _ in range(100):
+        y_hat = model(X_train)
+        loss = loss_fn(y_hat, y_train)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        # print(f"{loss:0.3f}")
+
+    # Подсчёт эффективности модели
+    score = (model(X_val).argmax(1) == y_val).sum() / len(y_val) * 100
+    results.append(int(score))
+
+results = np.array(results)
+
+save_info(info(results))
